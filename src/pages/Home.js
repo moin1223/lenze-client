d39d150580e4e56590929135b880fd557ca44c9a
moin1223 <moinpuccse@gmail.com>
2021-08-22 18:41:47 +0600

    desin

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,6 @@ import React from 'react';
 import About from '../components/Home/About/About';
 import Banner from '../components/Home/Banner/Banner';
 import Footer from '../components/Home/Footer/Footer';
-import Header from '../components/Home/Header/Header';
 import Services from '../components/Home/Services/Services';
 
 
